04af72f3bbad73c13a0ee60982509ecc7f2d12e8
Yun-D <yund_272@naver.com>
2025-01-10 02:16:55 +0900

    🎨Style: nav-bar 생성

--- src/App.js
+++ src/App.js
@@ -1,5 +1,9 @@
+import React from "react";
 import logo from "./logo.svg";
 import "./App.css";
+import { ReactComponent as IcnHome } from "./Assets/home.svg";
+import { ReactComponent as IcnRecord } from "./Assets/record.svg";
+import { ReactComponent as IcnWish } from "./Assets/wish.svg";
 
 function App() {
   return (
@@ -7,6 +11,19 @@ function App() {
       <div className="box-wrapper">
         <div className="content">
           <img src={logo} className="App-logo" alt="logo" />
+        </div>
+        <div className="nav-bar">
+          <a href="#home" className="nav-item">
+            <IcnHome className="nav-icon" />홈
+          </a>
+          <a href="#record" className="nav-item">
+            <IcnRecord className="nav-icon" />
+            기록
+          </a>
+          <a href="#wish" className="nav-item">
+            <IcnWish className="nav-icon" />
+            위시
+          </a>
         </div>
       </div>
     </div>
