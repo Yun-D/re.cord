ca396a5ec821e5e16144fec0d93f381771012d2d
Yun-D <yund_272@naver.com>
2025-01-24 02:07:04 +0900

    🎨Style: 홈 페이지 영역 미리 구성, 스타일 작업

--- src/App.js
+++ src/App.js
@@ -1,16 +1,30 @@
 import React from "react";
-import logo from "./logo.svg";
 import "./App.css";
 import Header from "./Components/Header";
 import Navbar from "./Components/Navbar";
 
 function App() {
+  let username = "username"; //TODO: 추후 백엔드 작업 시 변경요
+
   return (
     <div className="app-container">
       <div className="box-wrapper">
         <Header />
         <div className="content">
-          <img src={logo} className="App-logo" alt="logo" />
+          <h2>{username}님, 오늘은 어디로 가볼까요?</h2>
+
+          <div className="content-gap" />
+          <div>
+            <p className="text-title">최근 추가된 기록</p>
+
+            <div className="recentRecords" />
+          </div>
+          <div className="content-gap" />
+          <div>
+            <p className="text-title">평점 분포</p>
+
+            <div className="ratingSpread" />
+          </div>
         </div>
         <Navbar />
       </div>
