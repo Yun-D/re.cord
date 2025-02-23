3ca515598019122675bdc546c80988679538a4ad
Yun-D <yund_272@naver.com>
2025-02-24 01:06:57 +0900

    👷Chore: 레코드 생성 페이지 수정, 전체 box-sizing 적용

--- src/Pages/AddRecord.js
+++ src/Pages/AddRecord.js
@@ -1,20 +1,41 @@
 import React from "react";
 import record from "../Assets/recordpan.svg";
+import Button from "../Components/Button.js";
 import "./Record.css";
 
 const AddRecord = () => {
+  const colors = ["green", "pink", "yellow", "purple"];
+
   return (
     <div>
       <div className="content-align">
-        <div>
-          <img src={record} alt="레코드판" />
-        </div>
-        <p className="text-title">레코드 이름</p>
+        <img src={record} alt="레코드판" />
+      </div>
+
+      <div className="content-gap" />
+      <div>
+        <p className="text-subtitle">레코드 이름</p>
         <input
           type="text"
-          placeholder="장소를 모을 테마 이름을 정해주세요."
-        ></input>
-        <p className="text-title">색상</p>
+          placeholder=" 장소를 모을 테마 이름을 정해주세요."
+          maxLength={12} //FIXME: 적당한 값으로 변경해야함!
+          className="textinput"
+        />
+
+        <div className="content-gap" />
+        <p className="text-subtitle">색상</p>
+        <div className="row-direction">
+          {colors.map((color, idx) => (
+            <button className={`colorchip ${color}`}>
+              <div key={idx} />
+            </button>
+          ))}
+        </div>
+      </div>
+
+      <div className="content-align">
+        <div className="content-gap" />
+        <Button>기록 시작하기</Button>
       </div>
     </div>
   );
