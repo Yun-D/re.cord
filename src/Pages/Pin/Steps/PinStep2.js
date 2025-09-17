aca1e96eafba34f75c23fb09e07fb5e36c2efc9c
Yun-D <yund_272@naver.com>
2025-09-18 02:11:09 +0900

    🎨Style: 핀추가 스텝2 부분 UI 추가

--- src/Pages/Pin/Steps/PinStep2.js
+++ src/Pages/Pin/Steps/PinStep2.js
@@ -1,8 +1,33 @@
 // 핀 만들기 2단계 (장소 확인) -------------------------------
 import React from "react";
+import styles from "../../../Components/components.module.css";
+import "../AddPin.css";
+
+import SearchLocaItem from "../../../Components/SearchLocaItem";
+import { Link } from "react-router-dom";
 
 const PinStep2 = ({ nextStep }) => {
-  return <div></div>;
+  return (
+    <div className="contentAlign">
+      <div className="mapContainer" />
+
+      <SearchLocaItem onClick={nextStep} />
+      <div className="content-gap" />
+
+      <div className="underButtonArea">
+        <Link to="/wish">
+          <p style={{ color: "#727272" }}>위시로 등록하기</p>
+        </Link>
+
+        <button
+          className={`${styles.button} ${styles.longButton}`}
+          onClick={nextStep}
+        >
+          다음
+        </button>
+      </div>
+    </div>
+  );
 };
 
 export default PinStep2;
