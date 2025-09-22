e321d75943e8a7ccdc5371737a81dab03470831f
Yun-D <yund_272@naver.com>
2025-09-23 03:48:15 +0900

    🎨Style: 핀추가 스텝3 부분 UI 추가

--- src/Pages/Pin/Steps/PinStepFinal.js
+++ src/Pages/Pin/Steps/PinStepFinal.js
@@ -1,8 +1,55 @@
 // 핀 만들기 3단계 (메모 추가, 최종 등록) -------------------------------
 import React from "react";
+import ImageFrame from "../../../Components/ImageFrame";
+import styles from "../../../Components/components.module.css";
+import "../AddPin.css";
+import { useNavigate } from "react-router-dom";
 
 const PinStepFinal = () => {
-  return <div></div>;
+  const navigate = useNavigate();
+
+  const handleAddPlace = () => {
+    // TODO: 장소 등록 로직 구현. 기존 페이지 데이터 받아와 링크도 수정해야함
+    navigate("/recordDetail/");
+  };
+
+  return (
+    <div className="contentAlign" style={{ alignItems: "flex-start" }}>
+      <div
+        style={{
+          display: "flex",
+          flexDirection: "row",
+        }}
+      >
+        <ImageFrame />
+
+        <div className={styles.textWrapper} style={{ marginLeft: 20 }}>
+          <div className="text-subtitle" style={{ margin: 0 }}>
+            야옹 가챠
+          </div>
+          <div className="text-plain">주소주소주소 주소구 주소주로 232</div>
+        </div>
+      </div>
+
+      <div className="content-gap" />
+      <p className="text-subtitle">메모</p>
+      <textarea
+        // value={description}
+        // onChange={(e) => setDescription(e.target.value)}
+        placeholder=" 장소에 대해 기록하고 싶은 내용을 적어주세요."
+        className="textareaBox"
+      />
+
+      <div className="underButtonArea">
+        <button
+          className={`${styles.button} ${styles.longButton}`}
+          onClick={handleAddPlace}
+        >
+          장소 등록
+        </button>
+      </div>
+    </div>
+  );
 };
 
 export default PinStepFinal;
