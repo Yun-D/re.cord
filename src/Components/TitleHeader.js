9eff1f54b0f1ea5473954a477c323d203ffda595
Yun-D <yund_272@naver.com>
2025-11-14 03:09:34 +0900

    🐛Fix: 기록페이지들 헤더 뒤로가기 버튼 조건문 추가하여 뒤로가기 꼬임 개선

--- src/Components/TitleHeader.js
+++ src/Components/TitleHeader.js
@@ -1,16 +1,31 @@
 import React from "react";
-import { useNavigate } from "react-router-dom";
+import { useLocation, useNavigate } from "react-router-dom";
 import { ReactComponent as ArrowLeft } from "../Assets/arrow_back.svg";
 import { ReactComponent as MoreHorizontal } from "../Assets/more_vert.svg";
 import "./Header.css";
 
 const TitleHeader = ({ title, prevStep, step }) => {
+  const location = useLocation();
   const navigate = useNavigate();
 
+  const isPinDetailPage = (path) =>
+    path.includes("/recordDetail") && path.includes("/pinDetail");
+  const isRecordDetailPage = (path) =>
+    path.includes("/recordDetail") && !path.includes("/pinDetail");
+
   const handleBack = () => {
+    const currentPath = location.pathname;
+
     if (step > 0 && typeof prevStep === "function") {
       // 멀티스텝폼 UI일 경우
       prevStep();
+    } else if (isPinDetailPage(currentPath)) {
+      // 핀 디테일 페이지일 경우 /pinDetail 제거하고 /recordDetail로 이동
+      const basePath = currentPath.split("/pinDetail")[0];
+      navigate(basePath);
+    } else if (isRecordDetailPage(currentPath)) {
+      // 레코드 디테일 페이지일 경우 /record 페이지로 바로 이동
+      navigate("/record");
     } else {
       // 기본 뒤로가기
       navigate(-1);
