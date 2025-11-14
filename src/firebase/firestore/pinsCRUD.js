62fd2235b15c229d742962097cbad8f4d9dc36e2
Yun-D <yund_272@naver.com>
2025-11-15 00:02:13 +0900

    ♻️Refactor: pinsCRUD에 필요한 파이어스토어 경로생성 함수 공통으로 만들어 중복 제거, 가독성 개선

--- src/firebase/firestore/pinsCRUD.js
+++ src/firebase/firestore/pinsCRUD.js
@@ -10,8 +10,49 @@ import {
 } from "firebase/firestore";
 import { db } from "../firebase";
 
+// 컬렉션, 도큐먼트 참조 함수들 ------------------------------------------------------------------------
+// 사용자별 pins 서브컬렉션 참조
+function getPinsCollection(userId, recordId) {
+  return collection(db, "users", userId, "records", recordId, "pins");
+}
+
+// 특정 핀 도큐먼트 참조
+function getPinDoc(userId, recordId, pinId) {
+  return doc(db, "users", userId, "records", recordId, "pins", pinId);
+}
+
+// 사용자별 memos 서브컬렉션 참조
+function getMemosCollection(userId, recordId, pinId) {
+  return collection(
+    db,
+    "users",
+    userId,
+    "records",
+    recordId,
+    "pins",
+    pinId,
+    "memos"
+  );
+}
+
+// 특정 메모 도큐먼트 참조
+function getMemoDoc(userId, recordId, pinId, memoId) {
+  return doc(
+    db,
+    "users",
+    userId,
+    "records",
+    recordId,
+    "pins",
+    pinId,
+    "memos",
+    memoId
+  );
+}
+// --------------------------------------------------------------------------------------------------
+
 async function addPin(userId, newPin, recordId) {
-  const pinsRef = collection(db, "users", userId, "records", recordId, "pins"); // 사용자별 pins 서브컬렉션 참조
+  const pinsRef = getPinsCollection(userId, recordId);
   const newPinDocRef = doc(pinsRef);
 
   await setDoc(newPinDocRef, {
@@ -22,18 +63,11 @@ async function addPin(userId, newPin, recordId) {
 }
 
 async function fetchPins(userId, recordId) {
-  const recordsRef = collection(
-    db,
-    "users",
-    userId,
-    "records",
-    recordId,
-    "pins"
-  );
-  const recordSnap = await getDocs(recordsRef);
+  const pinRef = getPinsCollection(userId, recordId);
+  const pinSnap = await getDocs(pinRef);
 
-  if (!recordSnap.empty) {
-    return recordSnap.docs.map((doc) => ({ id: doc.id, ...doc.data() }));
+  if (!pinSnap.empty) {
+    return pinSnap.docs.map((doc) => ({ id: doc.id, ...doc.data() }));
   } else {
     console.log("No such document!");
     return [];
@@ -41,16 +75,7 @@ async function fetchPins(userId, recordId) {
 }
 
 async function addMemo(userId, recordId, pinId, newMemo) {
-  const memoRef = collection(
-    db,
-    "users",
-    userId,
-    "records",
-    recordId,
-    "pins",
-    pinId,
-    "memos"
-  );
+  const memoRef = getMemosCollection(userId, recordId, pinId);
   const newMemoDocRef = doc(memoRef);
 
   await setDoc(newMemoDocRef, {
@@ -59,15 +84,7 @@ async function addMemo(userId, recordId, pinId, newMemo) {
   });
   console.log("Memo added successfully");
 
-  const pinDocRef = doc(
-    db,
-    "users",
-    userId,
-    "records",
-    recordId,
-    "pins",
-    pinId
-  );
+  const pinDocRef = getPinDoc(userId, recordId, pinId);
   await updateDoc(pinDocRef, {
     lastUpdated: serverTimestamp(),
     memoCount: increment(1),
@@ -75,16 +92,7 @@ async function addMemo(userId, recordId, pinId, newMemo) {
 }
 
 async function fetchMemos(userId, recordId, pinId) {
-  const memoRef = collection(
-    db,
-    "users",
-    userId,
-    "records",
-    recordId,
-    "pins",
-    pinId,
-    "memos"
-  );
+  const memoRef = getMemosCollection(userId, recordId, pinId);
   const memoSnap = await getDocs(memoRef);
 
   if (!memoSnap.empty) {
@@ -96,31 +104,13 @@ async function fetchMemos(userId, recordId, pinId) {
 }
 
 async function deleteMemo(userId, recordId, pinId, memoId) {
-  const memoRef = doc(
-    db,
-    "users",
-    userId,
-    "records",
-    recordId,
-    "pins",
-    pinId,
-    "memos",
-    memoId
-  );
+  const memoRef = getMemoDoc(userId, recordId, pinId, memoId);
 
   try {
     await deleteDoc(memoRef);
     console.log(`${memoId} 문서 삭제 완료`);
 
-    const pinDocRef = doc(
-      db,
-      "users",
-      userId,
-      "records",
-      recordId,
-      "pins",
-      pinId
-    );
+    const pinDocRef = getPinDoc(userId, recordId, pinId);
     await updateDoc(pinDocRef, {
       memoCount: increment(-1),
     });
