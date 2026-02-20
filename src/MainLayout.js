4b8a0d27cbd2e1473ad28ec2b98f1f4421a88201
Yun-D <yund_272@naver.com>
2026-02-20 23:46:15 +0900

    🐛Fix: 유저 인증 완료 전 메인 레이아웃 렌더링 방지

--- src/MainLayout.js
+++ src/MainLayout.js
@@ -48,6 +48,11 @@ const MainLayout = () => {
     });
   }, []);
 
+  // 인증 체크가 완료될 때까지 로딩 화면 표시
+  if (!isAuthChecked) {
+    return <div>로딩중...</div>;
+  }
+
   return (
     <div className="app-container">
       <div className="box-wrapper">
